df469c0b46f90b55df12c33448043cc3cb2ef5ff
Line-em <alineemily@hotmail.com>
2022-04-12 22:22:13 +0100

    Testing Deploy w/ Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "https://line-em.github.io/react-travel-plans/"
+	base: "/react-travel-plans/"
 });
